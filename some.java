86cd6d685f909b18643c78aa48a346fce6716692
Sam <sambitxtreme@gmail.com>
2022-10-25 10:25:38 +0530

    Adding modification

--- some.java
+++ some.java
@@ -1,6 +1,6 @@
 public class myfirstclass{
     public static void main(String[] args)
     {
-                System.out.println(“Hello,World”);
+                System.out.println(“New,World”);
     }
     }
